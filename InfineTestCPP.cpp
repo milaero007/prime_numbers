af4a694199f38029721ed6baacdc8a97cb696bf7
Oleg Martchouk <oleg2@pc-120.home>
2018-09-05 17:21:05 +0200

    refactoring C++

--- InfineTestCPP.cpp
+++ InfineTestCPP.cpp
@@ -25,7 +25,7 @@ int countPrimeIn4Thread(int last) {
 
 //STEP 3
 std::set<int> getPrimeInSetSimple(int first, int last) {
-
+	  return std::set<int>();
 }
 
 std::set<int> getPrimeInSet4Thread(int last) {
@@ -33,29 +33,36 @@ std::set<int> getPrimeInSet4Thread(int last) {
 }
 //-- END OF STEP 3 --
 
+
+
+// -- TEST ENVIRONEMENT --
+#define START_TIMER()  { \
+		auto start = std::chrono::steady_clock::now();
+
+#define END_TIMER_AND_PRINT(step, nr) \
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( \
+				std::chrono::steady_clock::now() - start); \
+		std::cout << step << upper_limit << ":Primaries=" << nr << " Time=" << duration.count() << std::endl; \
+}
+
+
 int main(int argc, char** argv) {
 	int upper_limit = 1000000;
 	if (argc > 1) upper_limit = std::atoi(argv[1]);
 
 	// Step 1 : mono thread
-	auto start = std::chrono::steady_clock::now();
-	auto primaries_nr = countPrimeSimple(2, upper_limit);
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-			std::chrono::steady_clock::now() - start);
-	std::cout << "Step1 " <<upper_limit << ":Primaries=" << primaries_nr << " Time=" << duration.count() << std::endl;
+	START_TIMER();
+	auto prime_nr = countPrimeSimple(2, upper_limit);
+	END_TIMER_AND_PRINT("Step 1 ", prime_nr);
 
 	// Step 2 : Multi thread
-	start = std::chrono::steady_clock::now();
-	primaries_nr = countPrimeIn4Thread(upper_limit);
-	duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-			std::chrono::steady_clock::now() - start);
-	std::cout << "Step2 " << upper_limit << ":Primaries=" << primaries_nr << " Time=" << duration.count() << std::endl;
+	START_TIMER();
+	auto prime_nr = countPrimeIn4Thread(upper_limit);
+	END_TIMER_AND_PRINT("Step 2 ", prime_nr);
 
 	// Step 3 : Multi thread with a returned set
-	start = std::chrono::steady_clock::now();
+	START_TIMER();
 	auto primeSet = getPrimeInSet4Thread(upper_limit);
-	duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-			std::chrono::steady_clock::now() - start);
-	std::cout << "Step3 " << upper_limit << ":Primaries=" << primeSet.size() << " Time=" << duration.count() << std::endl;
+	END_TIMER_AND_PRINT("Step 3 ", primeSet.size());
 }
 
